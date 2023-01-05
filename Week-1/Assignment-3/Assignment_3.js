8546e1449f22daff0853701046e4de097b4e3ff6
林晉守 <orin@linjinshoudeMacBook-Air.local>
2023-01-05 23:43:18 +0800

    assignment-3_OK

--- Week-1/Assignment-3/Assignment_3.js
+++ Week-1/Assignment-3/Assignment_3.js
@@ -1,1 +1,32 @@
-console.log('hello');
+function countAandB(input) {
+    let count=0;
+    for (let i = 0;i<input.length;i++){
+        if (input[i]=='a'){
+            count+=1;
+        }else if(input[i]=='b'){
+            count+=1;
+        }else{}
+    }
+    return count
+}
+
+function toNumber(input) {
+    let number_list={
+        'a':1,
+        'b':2,
+        'c':3,
+        'd':4,
+        'e':5,
+        }
+    let trans_list=[];
+    for (let i = 0;i<input.length;i++){
+        trans_list.push(number_list[input[i]])
+    }
+    return trans_list
+}
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2)); // should print 0
+console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
